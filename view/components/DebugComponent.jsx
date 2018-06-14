c7bfa06fd5b644616f0b11a5a44bdfa8818e7d3b
Christopher Kaster <me@atomicptr.de>
2018-06-14 14:51:22 +0200

    extend debugcomponent to filter out certain fields

--- view/components/DebugComponent.jsx
+++ view/components/DebugComponent.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 
 export default class DebugComponent extends React.Component {
+
+    filteredData() {
+        // don't filter arrays
+        if(Array.isArray(this.props.data)) {
+            return this.props.data;
+        }
+
+        let ignores = this.props.ignore || [];
+
+        let newData = {};
+
+        for(let key in this.props.data) {
+            if(ignores.indexOf(key) === -1) {
+                newData[key] = this.props.data[key];
+            }
+        }
+
+        return newData;
+    }
+
     render() {
         if(localStorage.getItem("__db_developer_mode") !== "enabled") {
             return null;
         }
 
-        return <pre className="debug"><code>{JSON.stringify(this.props.data, null, "    ")}</code></pre>;
+        return <pre className="debug"><code>{JSON.stringify(this.filteredData(), null, "    ")}</code></pre>;
     }
 }
