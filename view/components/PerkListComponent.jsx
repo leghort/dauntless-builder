31a62b6b1032e15b3aab8e268c913cca6dbf11fa
Christopher Kaster <me@atomicptr.de>
2018-06-16 15:28:22 +0200

    color code perk levels

--- view/components/PerkListComponent.jsx
+++ view/components/PerkListComponent.jsx
@@ -3,9 +3,21 @@ import React from "react";
 import "../styles/components/perk-list.scss";
 
 export default class PerkListComponent extends React.Component {
+    getPerkLevelClass(perkValue) {
+        if(perkValue >= 1 && perkValue < 6) {
+            return "perk-ok perk-level-" + perkValue;
+        }
+
+        if(perkValue == 6) {
+            return "perk-max";
+        }
+
+        return "perk-overload";
+    }
+
     render() {
         let perks = this.props.perks.map(perk =>
-            <li key={perk.name}>+{perk.value} {perk.name}</li> );
+            <li key={perk.name} className={this.getPerkLevelClass(perk.value)}>+{perk.value} {perk.name}</li> );
 
         return <ul className="perk-list">
             {perks}
