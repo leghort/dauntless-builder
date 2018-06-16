e9fca0c89ec40f8a5f8559b4e0d67ab1cb5bbccc
Christopher Kaster <me@atomicptr.de>
2018-06-16 19:00:50 +0200

    add new index page

--- view/views/IndexView.jsx
+++ view/views/IndexView.jsx
@@ -5,17 +5,85 @@ import {Link} from "react-router-dom";
 export default class IndexView extends React.Component {
     render() {
         return <div>
-            <h1 className="title">Hello!</h1>
+            <h1 className="title">Welcome to Dauntless-Builder.com!</h1>
             <h2 className="subtitle">World!</h2>
 
             <p>Lorem ipsum dolor sit amet!</p>
 
-            <Link to="/b/new">
-                <button className="button is-dark">
-                    <i className="fas fa-plus"></i>&nbsp;
-                    Make a new build!
-                </button>
-            </Link>
+            <div className="featured">
+                <div className="columns">
+                    <div className="column">
+                        <Link to="/b/new">
+                            <div className="notification is-success new-build-tile feature-tile">
+                                <p className="title">Make a new build</p>
+                                <div className="feature-tile-background">
+                                    <i className="fas fa-plus"></i>
+                                </div>
+                            </div>
+                        </Link>
+                    </div>
+                </div>
+
+                <div className="columns">
+                    <div className="column">
+                        <Link to="/fav">
+                            <div className="tile notification is-danger feature-tile">
+                                <p className="title">Favorite Builds</p>
+                                <div className="feature-tile-background">
+                                    <i className="fas fa-heart"></i>
+                                </div>
+                            </div>
+                        </Link>
+                    </div>
+
+                    <div className="column">
+                        <a href="https://github.com/atomicptr/dauntless-builder" target="_blank">
+                            <div className="tile notification is-light feature-tile">
+                                <p className="title">GitHub</p>
+                                <div className="feature-tile-background">
+                                    <i className="fab fa-github"></i>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div className="column">
+                        <a href="https://buymeacoff.ee/atomicptr" target="_blank">
+                            <div className="tile notification is-light feature-tile">
+                                <p className="title">Support Me</p>
+                                <div className="feature-tile-background">
+                                    <i className="fas fa-coffee"></i>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div className="columns">
+                    <div className="column">
+                        <a href="https://reddit.com/r/dauntless" target="_blank">
+                            <div className="tile notification is-light feature-tile">
+                                <p className="title">/r/dauntless</p>
+                                <div className="feature-tile-background">
+                                    <i className="fab fa-reddit"></i>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div className="column">
+                        <a href="https://discord.gg/dauntless" target="_blank">
+                            <div className="tile notification is-light feature-tile">
+                                <p className="title">Dauntless Discord</p>
+                                <div className="feature-tile-background">
+                                    <i className="fab fa-discord"></i>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+            </div>
         </div>;
     }
 }
