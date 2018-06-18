3d9eea2552c096402035568d8cad4bfa142f3523
Christopher Kaster <me@atomicptr.de>
2018-06-18 14:26:45 +0200

    move modal cell list item into own component

--- view/components/ItemSelectModalComponent.jsx
+++ view/components/ItemSelectModalComponent.jsx
@@ -4,6 +4,7 @@ import Select from "react-select";
 require("react-select/dist/react-select.css");
 
 import DebugComponent from "./DebugComponent";
+import ModalCellListItemComponent from "./modal/ModalCellListItemComponent";
 
 export default class ItemSelectModalComponent extends React.Component {
     constructor(props, context) {
@@ -272,34 +273,11 @@ export default class ItemSelectModalComponent extends React.Component {
     }
 
     renderCell(item) {
-        let getPerkDescription = perk => {
-            if(perk in this.props.itemData.perks) {
-                return this.props.itemData.perks[perk].description;
-            }
-
-            return null;
-        };
-
-        let getDescriptions = variant => {
-            return Object.keys(variant.perks).map(perk => getPerkDescription(perk)).join(", ");
-        };
-
-        let variants = Object.keys(item.variants).map(v =>
-            <div key={v} className={"cell " + item.variants[v].rarity} onClick={() => this.onItemSelected("Cell", v)}>
-                <img src={"/assets/icons/perks/" + item.slot + ".png"} />
-                <div>
-                    <div className="cell-title">{v}</div>
-                    <div className="perks">{getDescriptions(item.variants[v])}</div>
-                </div>
-            </div>
-        );
-
-        return <React.Fragment key={item.name}>
-            <h3 className="subtitle cell-title-line">{item.name}</h3>
-            <div className="cells">
-                {variants}
-            </div>
-        </React.Fragment>
+        return <ModalCellListItemComponent
+            key={"Cell-" + item.name}
+            item={item}
+            itemData={this.props.itemData}
+            onSelected={this.onItemSelected.bind(this)} />;
     }
 
     render() {
