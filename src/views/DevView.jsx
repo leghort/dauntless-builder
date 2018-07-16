c0e8a5f5e53af32b59cfbd05ad68a96f54e73984
Christopher Kaster <me@atomicptr.de>
2018-07-16 12:08:21 +0200

    remove broken unique dev menu, add custom builds dev menu to test stuff

--- src/views/DevView.jsx
+++ src/views/DevView.jsx
@@ -2,30 +2,67 @@ import React from "react";
 
 import {Redirect, Link} from "react-router-dom";
 import DataUtil from "../utils/DataUtil";
+import BuildModel from "../models/BuildModel";
 
 export default class DevView extends React.Component {
     constructor(props, context) {
         super(props, context);
 
         this.tabs = [
+            "CustomBuilds",
             "Icons",
-            "BrokenUnique",
             "MissingPerks"
         ]
 
         this.tabTitles = {
+            "CustomBuilds": "Custom Builds",
             "Icons": "Items without Icons",
-            "BrokenUnique": "Broken Unique Effects",
             "MissingPerks": "Missing Perks"
         }
 
         this.state = {
-            currentTab: this.tabs[0]
+            currentTab: this.tabs[0],
+
+            customBuild: {
+                __version: 1,
+                weapon_name: "",
+                weapon_level: 0,
+                weapon_cell0: "",
+                weapon_cell1: "",
+                head_name: "",
+                head_level: 0,
+                head_cell: "",
+                torso_name: "",
+                torso_level: 0,
+                torso_cell: "",
+                arms_name: "",
+                arms_level: 0,
+                arms_cell: "",
+                legs_name: "",
+                legs_level: 0,
+                legs_cell: "",
+                lantern_name: "",
+                lantern_cell: ""
+            }
         }
     }
 
     componentDidMount() {
         this.setState({currentTab: this.props.match.params.tab || this.tabs[0]});
+    }
+
+    updateCustomBuildOption(field, value) {
+        let customBuild = this.state.customBuild;
+        customBuild[field] = value;
+        this.setState({customBuild});
+    }
+
+    openBuild() {
+        let build = new BuildModel(this.state.customBuild);
+
+        const buildId = build.serialize();
+
+        window.open(`https://www.dauntless-builder.com/b/${buildId}`);
     }
 
     renderTabs() {
@@ -36,6 +73,192 @@ export default class DevView extends React.Component {
                     <Link to={"/dev/" + tab}>{tab in this.tabTitles ? this.tabTitles[tab] : tab}</Link>
             </li>
         ))
+    }
+
+    renderCustomBuilds() {
+        const weapons = Object.keys(DataUtil.data().weapons).map(weapon => <option key={weapon} value={weapon}>{weapon}</option>);
+        const armours = Object.keys(DataUtil.data().armours).map(armour => ({name: armour, type: DataUtil.data().armours[armour].type}));
+        const heads = armours.filter(armour => armour.type === "Head").map(armour => <option key={armour.name} value={armour.name}>{armour.name}</option>);
+        const torsos = armours.filter(armour => armour.type === "Torso").map(armour => <option key={armour.name} value={armour.name}>{armour.name}</option>);
+        const arms = armours.filter(armour => armour.type === "Arms").map(armour => <option key={armour.name} value={armour.name}>{armour.name}</option>);
+        const legs = armours.filter(armour => armour.type === "Legs").map(armour => <option key={armour.name} value={armour.name}>{armour.name}</option>);
+        const lanterns = Object.keys(DataUtil.data().lanterns).map(lantern => <option key={lantern} value={lantern}>{lantern}</option>);
+
+        const cells = [];
+
+        for(let cellName in DataUtil.data().cells) {
+            let cell = DataUtil.data().cells[cellName];
+
+            for(let variant in cell.variants) {
+                cells.push(
+                    <option key={variant} value={variant}>[{cell.slot}] {variant}</option>
+                );
+            }
+        }
+
+        const levels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(level => <option key={level} value={level}>+{level}</option>);
+
+        return <React.Fragment>
+            <h3 className="title is-4">Custom Builds - Without constraints</h3>
+            <div className="columns">
+                <div className="column">
+                    <div className="select" style={{width: "100%"}}>
+                        <select style={{width: "100%"}} value={this.state.customBuild.weapon_name} onChange={e => this.updateCustomBuildOption("weapon_name", e.target.value)}>
+                            <option>Select Weapon...</option>
+                            {weapons}
+                        </select>
+                    </div>
+                </div>
+                <div className="column">
+                    <div className="select" style={{width: "100%"}}>
+                        <select style={{width: "100%"}} value={this.state.customBuild.weapon_level} onChange={e => this.updateCustomBuildOption("weapon_level", e.target.value)}>
+                            <option value={0}>Base</option>
+                            {levels}
+                        </select>
+                    </div>
+                </div>
+                <div className="column">
+                    <div className="select" style={{width: "100%"}}>
+                        <select style={{width: "100%"}} value={this.state.customBuild.weapon_cell0} onChange={e => this.updateCustomBuildOption("weapon_cell0", e.target.value)}>
+                            <option>Select Weapon Cell 1...</option>
+                            {cells}
+                        </select>
+                    </div>
+                </div>
+                <div className="column">
+                    <div className="select" style={{width: "100%"}}>
+                        <select style={{width: "100%"}} value={this.state.customBuild.weapon_cell1} onChange={e => this.updateCustomBuildOption("weapon_cell1", e.target.value)}>
+                            <option>Select Weapon Cell 2...</option>
+                            {cells}
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div className="columns">
+                <div className="column">
+                    <div className="select" style={{width: "100%"}}>
+                        <select style={{width: "100%"}} value={this.state.customBuild.head_name} onChange={e => this.updateCustomBuildOption("head_name", e.target.value)}>
+                            <option>Select head...</option>
+                            {heads}
+                        </select>
+                    </div>
+                </div>
+                <div className="column">
+                    <div className="select" style={{width: "100%"}}>
+                        <select style={{width: "100%"}} value={this.state.customBuild.head_level} onChange={e => this.updateCustomBuildOption("head_level", e.target.value)}>
+                            <option value={0}>Base</option>
+                            {levels}
+                        </select>
+                    </div>
+                </div>
+                <div className="column">
+                    <div className="select" style={{width: "100%"}}>
+                        <select style={{width: "100%"}} value={this.state.customBuild.head_cell} onChange={e => this.updateCustomBuildOption("head_cell", e.target.value)}>
+                            <option>Select head Cell...</option>
+                            {cells}
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div className="columns">
+                <div className="column">
+                    <div className="select" style={{width: "100%"}}>
+                        <select style={{width: "100%"}} value={this.state.customBuild.torso_name} onChange={e => this.updateCustomBuildOption("torso_name", e.target.value)}>
+                            <option>Select torso...</option>
+                            {torsos}
+                        </select>
+                    </div>
+                </div>
+                <div className="column">
+                    <div className="select" style={{width: "100%"}}>
+                        <select style={{width: "100%"}} value={this.state.customBuild.torso_level} onChange={e => this.updateCustomBuildOption("torso_level", e.target.value)}>
+                            <option value={0}>Base</option>
+                            {levels}
+                        </select>
+                    </div>
+                </div>
+                <div className="column">
+                    <div className="select" style={{width: "100%"}}>
+                        <select style={{width: "100%"}} value={this.state.customBuild.torso_cell} onChange={e => this.updateCustomBuildOption("torso_cell", e.target.value)}>
+                            <option>Select torso Cell...</option>
+                            {cells}
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div className="columns">
+                <div className="column">
+                    <div className="select" style={{width: "100%"}}>
+                        <select style={{width: "100%"}} value={this.state.customBuild.arms_name} onChange={e => this.updateCustomBuildOption("arms_name", e.target.value)}>
+                            <option>Select arms...</option>
+                            {arms}
+                        </select>
+                    </div>
+                </div>
+                <div className="column">
+                    <div className="select" style={{width: "100%"}}>
+                        <select style={{width: "100%"}} value={this.state.customBuild.arms_level} onChange={e => this.updateCustomBuildOption("arms_level", e.target.value)}>
+                            <option value={0}>Base</option>
+                            {levels}
+                        </select>
+                    </div>
+                </div>
+                <div className="column">
+                    <div className="select" style={{width: "100%"}}>
+                        <select style={{width: "100%"}} value={this.state.customBuild.arms_cell} onChange={e => this.updateCustomBuildOption("arms_cell", e.target.value)}>
+                            <option>Select arms Cell...</option>
+                            {cells}
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div className="columns">
+                <div className="column">
+                    <div className="select" style={{width: "100%"}}>
+                        <select style={{width: "100%"}} value={this.state.customBuild.legs_name} onChange={e => this.updateCustomBuildOption("legs_name", e.target.value)}>
+                            <option>Select legs...</option>
+                            {legs}
+                        </select>
+                    </div>
+                </div>
+                <div className="column">
+                    <div className="select" style={{width: "100%"}}>
+                        <select style={{width: "100%"}} value={this.state.customBuild.legs_level} onChange={e => this.updateCustomBuildOption("legs_level", e.target.value)}>
+                            <option value={0}>Base</option>
+                            {levels}
+                        </select>
+                    </div>
+                </div>
+                <div className="column">
+                    <div className="select" style={{width: "100%"}}>
+                        <select style={{width: "100%"}} value={this.state.customBuild.legs_cell} onChange={e => this.updateCustomBuildOption("legs_cell", e.target.value)}>
+                            <option>Select legs Cell...</option>
+                            {cells}
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div className="columns">
+                <div className="column">
+                    <div className="select" style={{width: "100%"}}>
+                        <select style={{width: "100%"}} value={this.state.customBuild.lantern_name} onChange={e => this.updateCustomBuildOption("lantern_name", e.target.value)}>
+                            <option>Select lantern...</option>
+                            {lanterns}
+                        </select>
+                    </div>
+                </div>
+                <div className="column">
+                    <div className="select" style={{width: "100%"}}>
+                        <select style={{width: "100%"}} value={this.state.customBuild.lantern_cell} onChange={e => this.updateCustomBuildOption("lantern_cell", e.target.value)}>
+                            <option>Select lantern Cell...</option>
+                            {cells}
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <br />
+            <button className="button is-primary is-large" onClick={() => this.openBuild()}>Open Build</button>
+        </React.Fragment>
     }
 
     renderIcons() {
@@ -93,37 +316,6 @@ export default class DevView extends React.Component {
         </React.Fragment>
     }
 
-    renderBrokenUnique() {
-        let weapons = Object.keys(DataUtil.data().weapons).filter(weaponName =>
-            ("wip_unique_effects" in DataUtil.data().weapons[weaponName])).sort((a, b) =>
-                a.localeCompare(b)).map(weaponName =>
-                    <li key={weaponName}><a>{weaponName}</a></li>);
-
-        let armours = Object.keys(DataUtil.data().armours).filter(armourName =>
-            ("wip_unique_effects" in DataUtil.data().armours[armourName])).sort((a, b) =>
-                a.localeCompare(b)).map(armourName =>
-                    <li key={armourName}><a>{armourName}</a></li>);
-
-        return <React.Fragment>
-            <h3 className="title is-4">Items with Broken Unique Effect</h3>
-
-            <div className="columns">
-                <div className="column">
-                    <h4 className="title is-6">Weapons</h4>
-                    <ul className="menu-list">
-                        {weapons}
-                    </ul>
-                </div>
-                <div className="column">
-                    <h4 className="title is-6">Armours</h4>
-                    <ul className="menu-list">
-                        {armours}
-                    </ul>
-                </div>
-            </div>
-        </React.Fragment>
-    }
-
     render() {
         if(!window.isDeveloperModeEnabled()) {
             return <Redirect to="/" />
@@ -133,8 +325,6 @@ export default class DevView extends React.Component {
             this.setState({currentTab: this.tabs[0]});
             return <Redirect to={"/dev/" + this.tabs[0]} />
         }
-
-        console.log("Render Tab", this.state.currentTab);
 
         return <section className="section">
             <h3 className="subtitle"><i className="fas fa-code"></i> Developer Menu</h3>
