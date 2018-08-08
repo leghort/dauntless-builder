372a222b596c8294cc4231c75c2414607a89fbcc
Christopher Kaster <me@atomicptr.de>
2018-08-08 12:48:24 +0200

    travis: only deploy if pushed on master

--- .travis/after_script.sh
+++ .travis/after_script.sh
@@ -1,7 +1,9 @@
-echo "### Deploy Changes"
-git config credential.helper "store --file=.git/credentials"
-echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+    echo "### Deploy Changes"
+    git config credential.helper "store --file=.git/credentials"
+    echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 
-export GIT_REMOTE_URL="https://${GH_TOKEN:-git}@github.com/atomicptr/dauntless-builder.git"
-export COMMIT_MESSAGE="[travis-ci] auto build $(date +'%F %H:%M %Z')"
-bash ./scripts/deploy.sh --commit
+    export GIT_REMOTE_URL="https://${GH_TOKEN:-git}@github.com/atomicptr/dauntless-builder.git"
+    export COMMIT_MESSAGE="[travis-ci] auto build $(date +'%F %H:%M %Z')"
+    bash ./scripts/deploy.sh --commit
+fi
