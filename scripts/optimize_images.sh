1cf2b5500d8b5b4fac28783a053f546be3e11639
Christopher Kaster <me@atomicptr.de>
2018-10-31 19:56:22 +0100

    only resize images that aren not already on the target size

--- scripts/optimize_images.sh
+++ scripts/optimize_images.sh
@@ -3,13 +3,17 @@
 function optimize_path () {
     for file in $(find $1 -name '*.png')
     do
-        # resize to 128px height
-        convert -resize 128x128 -gravity center -extent 128x128 -background none "$file" "$file"
+        if [[ $(identify -format "%wx%h" $file) != "128x128" ]]; then
+            # resize to 128px height
+            convert -resize 128x128 -gravity center -extent 128x128 -background none "$file" "$file"
+        fi
 
         # optmize images
-        optipng -O2 "$file"
+        optipng -silent -O2 "$file"
     done
 }
 
+echo "Optimizing images..."
 optimize_path ./assets/icons/armours
 optimize_path ./assets/icons/weapons
+echo "...done"
