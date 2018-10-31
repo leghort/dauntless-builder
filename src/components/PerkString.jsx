2ea19dbae89ac99b34b1216d305a026f4bdf919b
Christopher Kaster <me@atomicptr.de>
2018-10-31 21:02:03 +0100

    add icons to perks within item fields

--- src/components/PerkString.jsx
+++ src/components/PerkString.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
+import BuildModel from "../models/BuildModel";
 
 export default class PerkString extends React.Component {
 
     render() {
-        const perkList = this.props.perks.map(perk => `+${perk.value} ${perk.name}`);
+        const perkList = this.props.perks.map((perk, index) => <span key={perk.name} className="perk-string-items">
+            +{perk.value} <img src={"/assets/icons/perks/" + BuildModel.findPerkByName(perk.name).type + ".png"} /> {perk.name}
+            {index !== this.props.perks.length - 1 ? ", " : ""}
+        </span>);
 
         if(perkList.length > 0) {
-            return <div><strong>Perks</strong>: {perkList.join(", ")}</div>;
+            return <div><strong>Perks</strong>: {perkList}</div>;
         }
 
         return null;
