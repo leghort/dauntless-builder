b4f561302b73e247dcae3fe166b3b4693a0ca1ea
Leghort <leghort@outlook.fr>
2020-04-17 22:36:43 +0200

    1.3.14
    + hammer weapon filter translated

--- src/utility/ItemUtility.jsx
+++ src/utility/ItemUtility.jsx
@@ -40,7 +40,7 @@ export default class ItemUtility {
             case "Sword":
             case "Chain Blades":
             case "Hache":
-            case "Hammer":
+            case "Marteau":
             case "War Pike":
             case "Repeater":
             case "Aether Strikers":
