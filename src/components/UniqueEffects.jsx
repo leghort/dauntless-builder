9e3ae8a17e81631d53b4794e225aad8fb7d4f6a9
Christopher Kaster <me@atomicptr.de>
2020-09-26 10:58:38 +0200

    Add exotic weapon icons

--- src/components/UniqueEffects.jsx
+++ src/components/UniqueEffects.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import ItemUtility from "../utility/ItemUtility";
 import PropTypeUtility from "../utility/PropTypeUtility";
 import BuildModel from "../models/BuildModel";
+import LazyLoad from "react-lazy-load";
 
 export default class UniqueEffects extends React.Component {
 
@@ -17,6 +18,10 @@ export default class UniqueEffects extends React.Component {
                 <div key={uniqueEffect.name} className="item-title-wrapper">
                     <div className="item-wrapper unset-height">
                         <div className="item part-unique-effect no-cells">
+                            {uniqueEffect.icon ?
+                                (<LazyLoad className="image-icon-wrapper" width={100} height={100} offsetVertical={500}>
+                                    <img src={uniqueEffect.icon} />
+                                </LazyLoad>) : null}
                             <div className="item-data">
                                 <strong>{this.props.item.name} {uniqueEffect.title || "Unique Effect"}</strong>: <br/>
                                 {uniqueEffect.description}
